c780fd03876119387d1d900d1c24e82379945b98
jayseth <46541147+jayseth@users.noreply.github.com>
2023-10-30 11:14:48 +0530

    Initial commit

---
+++ hello.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+
+public class Program
+{
+  private static string apiKey = "myApiKeyValue";
+  public static void Main()
+  {
+    ExecuteQuery("Jay");
+    Console.WriteLine("Hello world, this is the new code");
+    System.String username = "testuser";
+    System.String password = "testpass";
+    CallApi(username, apiKey);
+  }
+  public static void ExecuteQuery(string userInput)    
+  {         
+    string query = "SELECT * FROM Users WHERE Username = '" + userInput + "'";         // Execute the query    
+  }
+  public static string DisplayMessage(string message)    
+  {         
+    return "<div>" + message + "</div>";     
+  }
+  public static void CallApi(string username, string key)
+    {
+        Console.WriteLine($"Calling API for user {username} with key {key}");
+    }
+  public static void Example()
+  {
+      var tempPath = Path.GetTempFileName();  // Noncompliant
+
+      using (var writer = new StreamWriter(tempPath))
+      {
+        writer.WriteLine("content");
+      }
+  }
+}
